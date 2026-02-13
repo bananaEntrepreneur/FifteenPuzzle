0dba1dc0bd560028ee58ca2325c64dd25b463d34
Artem K. <artem.kabatov22@gmail.com>
2026-02-13 12:27:35 +0300

    feat(FreezeMine): add basic implementation

--- src/units/FreezeMine.java
+++ src/units/FreezeMine.java
@@ -1,5 +1,29 @@
 package units;
 
+import field.Cell;
+
 public class FreezeMine extends Mine {
-    private void freeze() {}
+    private final int _freezeDuration;
+
+    public FreezeMine(int explosionDelay, int freezeDuration) {
+        super(explosionDelay);
+        this._freezeDuration = Math.max(0, freezeDuration);
+    }
+
+    @Override
+    protected void affectCell(Cell cell) {
+        freeze(cell);
+    }
+
+    private void freeze(Cell cell) {
+        if (cell == null) {
+            return;
+        }
+
+        for (Unit unit : cell.getAllUnits()) {
+            if (unit != null && unit.isActive()) {
+                unit.deactivate();
+            }
+        }
+    }
 }
