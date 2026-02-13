5af7373f5fbf0c5ea00ecce359d5870f24da22d0
Artem K. <artem.kabatov22@gmail.com>
2026-02-13 12:25:58 +0300

    feat(Mine): add basic implementation

--- src/units/Mine.java
+++ src/units/Mine.java
@@ -2,11 +2,74 @@ package units;
 
 import field.Cell;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mine extends Unit {
+    private final int _explosionDelay;
+    private int _timeToExplosion;
+
+    public Mine(int explosionDelay) {
+        super();
+        this._explosionDelay = Math.max(1, explosionDelay);
+        this._timeToExplosion = explosionDelay;
+    }
+
     @Override
-    protected boolean canBelongTo(Cell owner) {}
+    public boolean canBelongTo(Cell owner) {
+        return owner != null;
+    }
 
-    protected void explode() {}
+    public void tick() {
+        if (!isActive()) {
+            return;
+        }
+        
+        _timeToExplosion--;
+        if (_timeToExplosion <= 0) {
+            explode();
+        }
+    }
 
-    protected void applyEffect() {}
+    protected void explode() {
+        if (!isActive()) {
+            return;
+        }
+
+        Cell currentCell = owner();
+        if (currentCell == null) {
+            return;
+        }
+
+        applyEffect();
+
+        deactivate();
+    }
+
+    protected void applyEffect() {
+        Cell currentCell = owner();
+        if (currentCell == null) {
+            return;
+        }
+
+        affectCell(currentCell);
+
+        for (Cell neighbor : currentCell.getNeighborCells()) {
+            if (neighbor != null) {
+                affectCell(neighbor);
+            }
+        }
+    }
+
+    protected void affectCell(Cell cell) {
+
+    }
+
+    public int getTimeToExplosion() {
+        return _timeToExplosion;
+    }
+
+    public int getExplosionDelay() {
+        return _explosionDelay;
+    }
 }
