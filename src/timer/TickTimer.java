6c71c301b3f740abb0fde529c80f3afa124e948d
Artem K. <artem.kabatov22@gmail.com>
2026-02-19 00:00:39 +0300

    feat(Timer): add schedule task

--- src/timer/TickTimer.java
+++ src/timer/TickTimer.java
@@ -1,8 +1,11 @@
 package timer;
 
+import java.util.TimerTask;
+
 public class TickTimer extends MyTimer {
     private final int tickInterval;
     private int currentTickCount;
+    private TimerTask scheduledTask;
 
     public TickTimer(int tickInterval) {
         super();
@@ -10,20 +13,42 @@ public class TickTimer extends MyTimer {
         this.currentTickCount = 0;
     }
 
+    @Override
+    public void schedule(TimerTask task, long period) {
+        if (!isRunning && task != null) {
+            scheduledTask = task;
+            internalTimer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    tick();
+                }
+            }, 0, period);
+            isRunning = true;
+        }
+    }
+
     public void tick() {
         if (!isRunning) {
             return;
         }
-        
+
         currentTickCount++;
         if (currentTickCount >= tickInterval) {
             currentTickCount = 0;
-            onTick();
+            if (scheduledTask != null) {
+                scheduledTask.run();
+            }
         }
     }
 
-    protected void onTick() {
-
+    @Override
+    public void stop() {
+        if (isRunning) {
+            internalTimer.cancel();
+            internalTimer = new java.util.Timer();
+            isRunning = false;
+            currentTickCount = 0;
+        }
     }
 
     public int getTickInterval() {
