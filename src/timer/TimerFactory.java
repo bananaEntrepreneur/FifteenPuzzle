3ed95c024afac371d2ca7332bf13e9541262134c
Artem K. <artem.kabatov22@gmail.com>
2026-02-24 14:01:00 +0300

    feat(TimerFactory): add ticking

--- src/timer/TimerFactory.java
+++ src/timer/TimerFactory.java
@@ -1,11 +1,37 @@
 package timer;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class TimerFactory {
-    public MillisecondTimer getMillisecondTimer(long intervalMs) {
-        return new MillisecondTimer(intervalMs);
+    private final HashSet<MyTimer> _timers = new HashSet<>();
+
+    public MillisecondTimer getMillisecondTimer(long msInterval) {
+        MillisecondTimer msTimer = new MillisecondTimer(msInterval);
+        _timers.add(msTimer);
+        return msTimer;
     }
 
     public TickTimer getTickTimer(int tickInterval) {
-        return new TickTimer(tickInterval);
+        TickTimer msTimer = new TickTimer(tickInterval);
+        _timers.add(msTimer);
+        return msTimer;
+    }
+
+    public List<TickTimer> getTickTimers() {
+        List<TickTimer> tickTimers = new ArrayList<>();
+        for (MyTimer timer : _timers) {
+            if (timer instanceof TickTimer) {
+                tickTimers.add((TickTimer) timer);
+            }
+        }
+        return tickTimers;
+    }
+
+    public void tickTimers() {
+        for (TickTimer tickTimer : getTickTimers()) {
+            tickTimer.tick();
+        }
     }
 }
